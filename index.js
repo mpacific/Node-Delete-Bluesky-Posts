d700f1110df0b22b7dddf426c8ba0714d08faf09
Mike Pacific <mike.pacific@gmail.com>
2024-10-26 13:48:50 -0700

    Add linting

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 require('dotenv').config()
 const Bluesky = require('./lib/bluesky')
-const _ = require('lodash')
 
-const runScript = async function() {
+const runScript = async function () {
   if (process.env.BLUESKY_DELETE_POSTS === 'true') {
     await Bluesky.deletePosts()
   }
-  
+
   if (process.env.BLUESKY_DELETE_LIKES === 'true') {
     await Bluesky.deleteLikes()
-  }  
+  }
 }
 
-runScript();
+runScript()
