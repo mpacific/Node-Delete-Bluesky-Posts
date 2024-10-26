4d76ca3bdbff66b1e2f3f0222cdf53706b17725f
Mike Pacific <mike.pacific@gmail.com>
2024-10-26 13:56:46 -0700

    Convert bluesky.js to a class

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require('dotenv').config()
 const Bluesky = require('./lib/bluesky')
 
 const runScript = async function () {
+  const bsky = new Bluesky()
+
   if (process.env.BLUESKY_DELETE_POSTS === 'true') {
-    await Bluesky.deletePosts()
+    await bsky.deletePosts()
   }
 
   if (process.env.BLUESKY_DELETE_LIKES === 'true') {
-    await Bluesky.deleteLikes()
+    await bsky.deleteLikes()
   }
 }
 
